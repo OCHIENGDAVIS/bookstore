ffa7dcce772d254491d183006c5f35f4cb1edfda
davis <davisochieng4@gmail.com>
2024-09-07 11:28:35 +0300

    Add book related tests

--- books/models.py
+++ books/models.py
@@ -12,7 +12,7 @@ class Book(models.Model):
     )
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    prince = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.title
